7801db9c1ce0abac67a9156933be23911bd8170b
Jhonjaxde <jjarteaga@uce.edu.ec>
2024-11-15 15:49:30 +0000

    tarea 4

---
+++ js/jsLoginExterno.js
@@ -0,0 +1,19 @@
+function cambiarTexto(idElemento, valor) {
+    document.getElementById(idElemento).innerText = valor;
+}
+
+function cambiarColor(idElemento, valor) {
+    document.getElementById(idElemento).style.color = valor;
+}
+
+function cambiarTamano(idElemento, valor) {
+    document.getElementById(idElemento).style.fontSize = valor;
+}
+
+function limpiarCampo(idElemento) {
+    document.getElementById(idElemento).value = '';
+}
+
+function agregarElemento(idElemento, elementoHTML) {
+    document.getElementById(idElemento).innerHTML += elementoHTML;
+}
