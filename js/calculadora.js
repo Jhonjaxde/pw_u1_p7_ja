e9c580d9b9ed9343adcbb9c530d005ea9cd46862
Jhonjaxde <jjarteaga@uce.edu.ec>
2024-11-22 22:19:35 +0000

    tarea5

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,45 +1,56 @@
-var num1 =0;
-var num2 =0; 
-var operacion ='';
-var valorFinal =0;
+var num1 = 0;
+var num2 = 0;
+var operacion = '';
+
 function setearValor(valor){
     let valorActual =document.getElementById('id-dsplay').innerText;
     console.log(valorActual);
-    letdocument.getElementById('id-dsplay').innerText=valor;
+     document.getElementById('id-dsplay').innerText=valor;
    
 }
 
 function setearValorConcatenado(valor){
     let valorActual =document.getElementById('id-dsplay').innerText;
     console.log(valorActual);
-    letdocument.getElementById('id-dsplay').innerText=valor+valorActual;
+     document.getElementById('id-dsplay').innerText=valorActual+valor;
    
 }
-function recibirValor(valor){
-        console.log(valor)
-        if(num1===null){
-            num1=valor
-
-        }else{
-            num2=valor
-        }
-        console.log(num1)
+function recibirValor(valor) {
+    if (num1 === 0) {
+        num1 = parseFloat(valor);
+        setearValor(valor);
+    } else {
+        num2 = parseFloat(valor);
+        setearValorConcatenado(valor);
     }
+    setearValorConcatenado(valorFinal)
+}
 
-   
+function evaluarOperacion(op) {
+    operacion = op;
+    setearValorConcatenado(op);
+}
 
-
-    function evaluarOpe(op){
-        operacion= op
-    }
-
-function igual(){
+function igual() {
     let valorFinal;
-    if(operacion==='+'){
-        valorFinal=num1+num2
-    }else if(operacion==='-'){
-        valorFinal=num1-num2
+    if (operacion === '+') {
+        valorFinal = num1 + num2;
+    } else if (operacion === '-') {
+        valorFinal = num1 - num2;
+    } else if (operacion === 'x') {
+        valorFinal = num1 * num2;
+    } else if (operacion === '%') {
+        valorFinal = num1 / num2;
     }
     setearValor(valorFinal);
+    num1 = valorFinal;
+    num2 = null;
+    operacion = null;
 }
 
+function limpiar() {
+    num1 = null;
+    num2 = null;
+    operacion = null;
+    setearValor('');
+}
